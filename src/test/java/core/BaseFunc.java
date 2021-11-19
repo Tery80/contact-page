fe70d5f0ed5a22d096177ec4af76b59c6c5a6bcc
jekaterina.kuzmenko <sjukroshka@gmail.com>
2021-11-19 17:13:15 +0200

    ContactForm

--- src/test/java/core/BaseFunc.java
+++ src/test/java/core/BaseFunc.java
@@ -5,20 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
-
 public class BaseFunc {
     private WebDriver driver;
-
-    private final WebDriverWait wait = new WebDriverWait(this.driver, 10);
+    private final WebDriverWait wait = new WebDriverWait(driver, 10);
 
     public BaseFunc() {
         System.setProperty("webdriver.chrome.driver", "c:/temp/chromedriver.exe");
